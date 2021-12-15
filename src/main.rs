ea35ac98c270afe52a85489165f88ccb8cee0e36
dajamante <aimaiga2@gmail.com>
2021-12-15 12:06:07 +0100

    cleaned functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,44 @@
 use cargo_metadata::Message;
 use log::info;
+use std::collections::HashMap;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::path::Path;
 use std::process::{Command, Stdio};
+
+fn read_data() -> HashMap<String, i32> {
+    if let Ok(error_map) = File::open(Path::new("error_map.json")) {
+        println!("{:#?}", Path::new("error_map.json"));
+        println!("error map: {:#?}", error_map);
+        serde_json::from_reader(&error_map).unwrap()
+    } else {
+        Default::default()
+    }
+}
+
+fn write_data(map: &HashMap<String, i32>) {
+    //let serialized = serde_json::to_string(&map).unwrap();
+    //serde_json::to_writer(error_map, serialized.as_bytes())
+    let mut error_map = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .append(false)
+        .open("error_map.json")
+        .unwrap();
+
+    //error_map.write_all(serialized.as_bytes()).unwrap();
+    let serialized = serde_json::to_writer(&error_map, &map);
+    println!("Serialized: {:#?}", serialized.unwrap());
+}
+
+/// To do: fix path manifest to make to take any path
+/// Make a persistent file to store errors
 fn main() {
     env_logger::init();
+
+    info!("Getting the json file to reccord errors.");
+    //println!("{:#?}", error_map);
 
     info!("Trying to read from another rust program");
     let mut command = Command::new("cargo")
@@ -26,8 +62,16 @@ fn main() {
             _ => {} //println!("something"), // Unknown message
         }
     }
-    println!("Error code 🍍: {}", supererror);
-    let output1 = command.wait().expect("Couldn't get cargo's exit status");
+    println!("GAMEPILER found error with code 🍍: {}", supererror);
+
+    command.wait().expect("Couldn't get cargo's exit status");
+
+    let mut error_map: HashMap<String, i32> = read_data();
+    let stat = error_map.entry(supererror).or_insert(0);
+    *stat += 1;
+    write_data(&error_map);
+    // save into file
+    println!("Greeting file: {:#?}", &error_map);
 
     command = Command::new("cargo")
         .args(&[
@@ -38,5 +82,5 @@ fn main() {
         .spawn()
         .unwrap_or_else(|e| panic!("failed to execute process: {}", e));
 
-    let output2 = command.wait().expect("Couldn't get cargo's exit status");
+    command.wait().expect("Couldn't get cargo's exit status");
 }
